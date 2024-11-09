8fb5dd9b0c7a1453f4c8f9e160ef7ba683c3c39f
李力士 <lilishi@chinasofti.com>
2024-11-09 10:58:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -37,7 +37,7 @@ let urls = [
 ];
 let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 // let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
-let subconfig = "https://raw.githubusercontent.com/lls94/ACL4SSR/master/Clash/config/lls.ini"; //订阅配置文件
+let subconfig = "https://raw.githubusercontent.com/lls94/ACL4SSR/master/Clash/config/lls-pc.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
